423c66e21bd6acb042bf6fe7ef10b2bfbb8e9838
huang wei <1119765689hw@gmail.com>
2023-12-03 12:43:38 +0800

    finish the world_time demo

---
+++ world_time/lib/pages/choose_location.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[400],
+          title: Text("Choose a Location"),
+          titleTextStyle: TextStyle(color: Colors.white),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Text("Choose Location screen"),
+      ),
+    );
+  }
+}
