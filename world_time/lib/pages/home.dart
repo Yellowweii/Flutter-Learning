ef0ad37b8a3376caaa151971d79aa97a5a9901f8
Wei  Huang <138266079+Yellowweii@users.noreply.github.com>
2023-12-04 10:31:25 +0800

    finish some function of the project (#2)
    * finish the world_time demo
    
    * finish some function of the project

--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
+  const Home({super.key});
+
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  Object? data;
   @override
   Widget build(BuildContext context) {
+    data = ModalRoute.of(context)?.settings.arguments;
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -16,8 +20,8 @@ class _HomeState extends State<Home> {
                 onPressed: () {
                   Navigator.pushNamed(context, '/location');
                 },
-                icon: Icon(Icons.edit_location),
-                label: Text("Edit Location"))
+                icon: const Icon(Icons.edit_location),
+                label: const Text("Edit Location"))
           ],
         ),
       ),
