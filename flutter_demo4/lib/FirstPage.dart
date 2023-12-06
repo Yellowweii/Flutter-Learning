5e1ddb7df817150f4bb84fe75f4e40e60a9e19d2
huang wei <1119765689hw@gmail.com>
2023-12-06 12:00:16 +0800

    finish some functions of the project

---
+++ flutter_demo4/lib/FirstPage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo4/Profile.dart';
+import 'package:flutter_demo4/Settings.dart';
+import 'package:flutter_demo4/Home.dart';
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  List list = [
+    Home(),
+    Profile(),
+    Settings(),
+  ];
+  int _currentIndex = 0;
+  void onTappedBar(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple[200],
+        title: Text("First Page"),
+        centerTitle: true,
+      ),
+      body: list[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTappedBar,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter_demo4/lib/Settings.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text("Settings Page"),
+    ));
+  }
+}
