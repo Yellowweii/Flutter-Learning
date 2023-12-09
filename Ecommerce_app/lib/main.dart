81a1aa165822236b5f150f56ada0d92f95284f36
Wei  Huang <138266079+Yellowweii@users.noreply.github.com>
2023-12-09 17:30:31 +0800

    make the project responsive (#16)
    * finish the world_time demo
    
    * finish some function of the project
    
    * little change
    
    * finish the world_time project
    
    * init the project
    
    * finish some functions of the project
    
    * finish the project
    
    * refresh the branch
    
    * init the project
    
    * little change
    
    * finish the todoApp
    
    * init the project
    
    * finish some functions of the project
    
    * finish the whole app
    
    * make the project responsive

--- Ecommerce_app/lib/main.dart
+++ Ecommerce_app/lib/main.dart
@@ -2,18 +2,26 @@ import 'package:ecommerce_app/pages/cart_page.dart';
 import 'package:ecommerce_app/pages/home_page.dart';
 import 'package:ecommerce_app/pages/intro_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'model/cart_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
-      create: (context) => CartModel(),
-      child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: IntroPage(),
-          routes: {
-            '/intro': (context) => IntroPage(),
-            '/home': (context) => HomePage(),
-            '/cart': (context) => CartPage(),
-          })));
+    create: (context) => CartModel(),
+    child: ScreenUtilInit(
+        designSize: Size(390, 844),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: IntroPage(),
+              routes: {
+                '/intro': (context) => IntroPage(),
+                '/home': (context) => HomePage(),
+                '/cart': (context) => CartPage(),
+              });
+        }),
+  ));
 }
