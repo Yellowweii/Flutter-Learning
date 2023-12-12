3475665d1725a54c0a120de1b61108e53f3a3e42
Wei  Huang <138266079+Yellowweii@users.noreply.github.com>
2023-12-12 15:13:44 +0800

    since the incompleteness of the youtube video, I will not write my code next (#24)
    * finish the world_time demo
    
    * finish some function of the project
    
    * little change
    
    * finish the world_time project
    
    * init the project
    
    * finish some functions of the project
    
    * finish the project
    
    * refresh the branch
    
    * init the project
    
    * little change
    
    * finish the todoApp
    
    * init the project
    
    * finish some functions of the project
    
    * finish the whole app
    
    * make the project responsive
    
    * init the project
    
    * finish the search module
    
    * finsih the category module
    
    * finish the recommendation module
    
    * finish the popular module
    
    * init the project
    
    * complete some functions of welcome page
    
    * since the incompleteness of the youtube video, I will not write my code next

---
+++ flutter_demo7/lib/pages/signin_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo7/widgets/custom_scaffold.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(child: Text("Sign in"));
+  }
+}
---
+++ flutter_demo7/lib/pages/signup_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo7/widgets/custom_scaffold.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(child: Text("Sign up"));
+  }
+}
