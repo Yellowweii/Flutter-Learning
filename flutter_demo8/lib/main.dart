094c40c3cde0f4752063d4d28bf4d4bf7ee7bb5c
Wei  Huang <1119765689hw@gmail.com>
2023-12-16 22:00:14 +0800

    finish some parts of flutter-demo9 (#26)
    * finish the world_time demo
    
    * finish some function of the project
    
    * little change
    
    * finish the world_time project
    
    * init the project
    
    * finish some functions of the project
    
    * finish the project
    
    * refresh the branch
    
    * init the project
    
    * little change
    
    * finish the todoApp
    
    * init the project
    
    * finish some functions of the project
    
    * finish the whole app
    
    * make the project responsive
    
    * init the project
    
    * finish the search module
    
    * finsih the category module
    
    * finish the recommendation module
    
    * finish the popular module
    
    * init the project
    
    * complete some functions of welcome page
    
    * since the incompleteness of the youtube video, I will not write my code next
    
    * create flutter-demo8
    
    * init the project
    
    * finish some parts of flutter-demo9

--- flutter_demo8/lib/main.dart
+++ flutter_demo8/lib/main.dart
@@ -1,30 +1,74 @@
-import 'package:authing_sdk/authing.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  Authing.init("657945fd00b5ee9cfb1f52ca", "657946b97de38ec3e1ccf99b");
   runApp(MaterialApp(home: Home()));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(
-      child: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraint) {
-          return OutlinedButton(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello, AppBar"),
+        centerTitle: true,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: Icon(Icons.menu),
               onPressed: () {
-                print(constraint);
+                Scaffold.of(context).openDrawer();
               },
-              child: Text(
-                "click me",
-                style: GoogleFonts.aboreto(),
-              ));
-        },
+            );
+          },
+        ),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.share))],
+        bottom: PreferredSize(
+          preferredSize: Size.square(12),
+          child: Text("Hello"),
+        ),
+        elevation: 0,
       ),
-    ));
+      drawer: Drawer(),
+      body: SafeArea(
+          child: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            ListView.builder(itemBuilder: (BuildContext context, int index) {
+              return ListTile();
+            })
+          ],
+        ),
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.business), label: "Business"),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: "School")
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        fixedColor: Colors.red,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
